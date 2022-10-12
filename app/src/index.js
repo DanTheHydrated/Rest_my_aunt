d948eafb97bef418463cd6f85356eb440eb7e494
Dan W <daniel.winn@student.awesomeinc.org>
2022-10-12 17:35:49 +0000

    fresh start

---
+++ app/src/index.js
@@ -0,0 +1,12 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+  <div>
+    <h1>Egg!</h1>
+  </div>
+  </StrictMode>
+
+);
