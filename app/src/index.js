a00f7fd1c5b5a2461f9d699377fbf5a063d1d289
Dan W <daniel.winn@student.awesomeinc.org>
2022-10-12 20:57:26 +0000

    broken at the moment

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,26 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import calling from './calling';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
   <div>
     <h1>Egg!</h1>
-  </div>
-  </StrictMode>
 
+  </div>
+  
+  </StrictMode>
 );
+
+
+
+
+
+
+
+
+
+
+
+//{/* <button onClick={calling()}> Get Data! </button> */}
